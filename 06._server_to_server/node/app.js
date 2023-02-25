7b34a32b70fae6e01d3fca635d235a2a84a55f86
erhan basaran <e.basaran.111@gmail.com>
2023-02-25 14:33:31 +0100

    from class 21_02

--- 06._server_to_server/node/app.js
+++ 06._server_to_server/node/app.js
@@ -7,11 +7,18 @@ app.get('/date', (req, res) => {
 
 //for at de to services kan kommunikere
 app.get('/datefromfastapi', async(req, res) => {
-    /*task get date from fastapi. Complete the folowing:*/
-    fetch('http://localhost:8000/date')
+/*task get date from fastapi. Complete the folowing:*/
+    const response = await fetch('http://127.0.0.1:8000/date');
+    const date = await response.json();
+    res.send(date);
+});
 
+//for at kunne tilgå ngrok
+app.get('/test', (req, res) => {
+    res.send("Hello from erhan");
 });
 
 const PORT = 8080;
-app.listen(PORT,() => console.log("Server runnig on port 8080", PORT));
+app.listen(PORT,() => console.log("Server runnig on port ", PORT));
+
 
---
+++ 07._dotenv/python/main.py
@@ -0,0 +1,5 @@
+# Example 1
+from dotenv import load_dotenv, dotenv_values
+dotenv_values = dotenv_values()
+print(dotenv_values.get("MY_SECRET"))
+
