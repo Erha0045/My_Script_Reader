0426304e721c984bc5da441b39f446508dad5d00
erhan basaran <e.basaran.111@gmail.com>
2023-02-26 22:05:24 +0100

    path edited

--- 02.1_file_parsers/python/main.py
+++ 02.1_file_parsers/python/main.py
@@ -2,11 +2,11 @@
 
 from my_modules import ClassParser
 
-#ClassParser().read_and_parse_file("data_formats/sample.json")
-#ClassParser().read_and_parse_file("data_formats/sample.txt")
-# ClassParser().read_and_parse_file("data_formats/sample.csv")
-# ClassParser().read_and_parse_file("data_formats/sample.xml")
-ClassParser().read_and_parse_file("data_formats/sample.yaml")
+#ClassParser().read_and_parse_file("02._data_formats/sample.json")
+#ClassParser().read_and_parse_file("02._data_formats/sample.txt")
+ClassParser().read_and_parse_file("02._data_formats/sample.csv")
+# ClassParser().read_and_parse_file("02._data_formats/sample.xml")
+# ClassParser().read_and_parse_file("02._data_formats/sample.yaml")
 
 # def my_txt_file_parser(file_path):
 #     with open(file_path, 'r') as f:
